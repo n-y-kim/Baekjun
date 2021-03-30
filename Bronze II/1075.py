818a82a571a0c8f2f3a609a97c2229e33311a642
n-y-kim <nyn2265@gmail.com>
2021-03-30 15:50:53 +0900

    1075.py

---
+++ Bronze II/1075.py
@@ -0,0 +1,28 @@
+from sys import stdin
+
+N = stdin.readline()
+F = int(stdin.readline())
+
+N_list = list(N)
+N_list.pop()
+
+N_front = N_list[:-2]
+
+front_length = len(N_front)
+front = 0
+for i in range(front_length):
+    front += int(N_front.pop()) * 10**i
+
+front *= 100
+
+minNum = 0
+for i in range(99):
+    temp = front + i
+    if temp != int(N) and temp%F == 0: 
+        minNum = i
+        break
+
+if minNum<10:
+    print(0, end='')
+    print(minNum)
+else: print(minNum)
