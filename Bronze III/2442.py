cae14e8c493025f2821b42ffa422f85df59acd5f
n-y-kim <nyn2265@gmail.com>
2021-06-30 22:50:44 +0900

    2442

---
+++ Bronze III/2442.py
@@ -0,0 +1,8 @@
+from sys import stdin
+n = int(stdin.readline())
+for i in range(1, n+1):
+    for j in range(n-i):
+        print(" ", end='')
+    for j in range(2*i-1):
+        print("*", end='')
+    print()
